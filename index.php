1b4b5175332e03c74829a55c9a6d66af9d6c6e9f
lindseyemaddox <lindseyemaddox@gmail.com>
2015-07-25 22:13:10 -0500

    splash page and icon fonts

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 
 <section>
 
+<img style="display:block; margin: 0 auto; padding-top: 10%;" src="/_assets/img/logo.png" alt="Ready Set Go logo" title="logo for Ready Set Go Collective">
+
+<p style="text-align: center; color: black; padding-top: 50px; font: 14px 'verdana';">Full site coming soon. In the meantime, <a href="mailto:dev@readysetgo.agency">drop us a line</a>!</p>
+
 </section>
 
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/footer.php'); // footer, close body and html?>
